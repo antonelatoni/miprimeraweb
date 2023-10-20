ffac6fe2cd73f9fa895eddc3de009ed2124eb820
antonelatoni <148506555+antonelatoni@users.noreply.github.com>
2023-10-19 22:06:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//alert("Hola este es mi Javascript");
+
+let nombre = "Antonela";
+
+console.log (nombre);
+
+let contenidoTitulo = "Nombre";
+
+let titulo = document.querySelector (".logo .fuente");
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES//
+
+let textoTitulo = titulo.innerText;
+console.log (textoTitulo);
